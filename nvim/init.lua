76115818ea3ecfb9ac1d0f402f5bdf576bacf363
Ronald So <ronald.so1996@gmail.com>
2024-08-14 22:19:50 -0700

    harpoon

--- nvim/init.lua
+++ nvim/init.lua
@@ -14,13 +14,67 @@ vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup("plugins")
 
+-- telescope
+
+local telescope = require("telescope")
+telescope.setup{
+  defaults = {
+    layout_strategy = "vertical"
+  }
+}
+
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
 
+-- neotree
+
 vim.keymap.set('n', '<leader>e', '<Cmd>Neotree toggle<CR>', {silent = true})
+
+-- harpoon
+
+local harpoon = require("harpoon")
+harpoon:setup({
+  settings = {
+    save_on_toggle = true,
+    sync_on_ui_close = true,
+  },
+})
+
+vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
+vim.keymap.set("n", "<leader>r", function() harpoon:list():remove() end)
+vim.keymap.set("n", "<leader>c", function() harpoon:list():clear() end)
+
+vim.keymap.set("n", "<leader>1", function() harpoon:list():select(1) end)
+vim.keymap.set("n", "<leader>2", function() harpoon:list():select(2) end)
+vim.keymap.set("n", "<leader>3", function() harpoon:list():select(3) end)
+vim.keymap.set("n", "<leader>4", function() harpoon:list():select(4) end)
+
+vim.keymap.set("n", "<leader>n", function() harpoon:list():next({ui_nav_wrap = true}) end)
+
+local conf = require("telescope.config").values
+local function toggle_telescope(harpoon_files)
+    local file_paths = {}
+    for _, item in ipairs(harpoon_files.items) do
+        table.insert(file_paths, item.value)
+    end
+
+    require("telescope.pickers").new({}, {
+        prompt_title = "Harpoon",
+        finder = require("telescope.finders").new_table({
+            results = file_paths,
+        }),
+        previewer = conf.file_previewer({}),
+        sorter = conf.generic_sorter({}),
+    }):find()
+end
+
+vim.keymap.set("n", "<C-e>", function() toggle_telescope(harpoon:list()) end,
+    { desc = "Open harpoon window" })
+
+-- options
 
 vim.opt.clipboard = "unnamedplus"
 vim.opt.relativenumber = true
